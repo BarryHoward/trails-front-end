23e465e5f6b09e42de90047d6485516c1fe1c112
Barry Howard <BarryLeeHoward@gmail.com>
2016-11-30 11:47:48 -0500

    updated view controller

--- src/js/controllers/trailView.js
+++ src/js/controllers/trailView.js
@@ -1,55 +1,38 @@
-const mapId = 'trailViewMap';
+
+const mapId = "trailViewMap"
 
 function TrailViewController (TrailsService, $stateParams) {
-  let vm= this;
-  vm.TrailsService = TrailsService;
-  vm.getTrail = getTrail;
-  vm.loadMarker = loadMarker;
-  vm.drawLine = drawLine;
-  vm.placeMarker = placeMarker;
+  let vm = this;
 
+  vm.getTrail = getTrail;
+  
   function init () {
-    let trail_id = $stateParams.id;
+    let trail_id = $stateParams.id
     vm.markers = [];
 
-    TrailsService.getMap(mapId, trail_id).then(function (map) {
+    TrailsService.getMap(mapId).then(function (map) {
       vm.map = map;
 
       vm.getTrail(trail_id);
-      
-    });
-
-
-    var elevator = new google.maps.ElevationService;
-
+    })
   }
 
   init();
 
+  
   function getTrail(id){
     TrailsService.getTrail(id).then(
       (resp) => {
         resp.data.waypoints.forEach(function (waypoint) {
-          vm.loadMarker(waypoint);
+          TrailsService.loadMarker(vm.map, vm.markers, waypoint, false)
         });
-        vm.drawLine();
+        TrailsService.drawLine(vm.map, vm.markers)
         vm.trailTitle = resp.data.trailInfo.title;
       }, (reject) => {
           console.log(reject)
       });
   }
 
-  function loadMarker(waypoint){
-    TrailsService.loadMarker(vm.map, vm.markers, waypoint)
-  }
-
-  function drawLine(){
-    TrailsService.drawLine(vm.map, vm.markers)
-  }
-
-  function placeMarker(event){
-    TrailsService.placeMarker(vm.markers, vm.map, event);
-  }
 
 
 }
