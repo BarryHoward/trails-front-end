2481e012b1f57d28ce1f6cb2e049f4381a5722b5
Jack Storrs <jackstorrs8@gmail.com>
2016-12-02 12:10:40 -0500

    Only uncomment methods on the UserController that are functional.

--- src/js/services/users.js
+++ src/js/services/users.js
@@ -6,79 +6,79 @@ function UsersService ($http, $cookies) {
   let vm = this;
 
   vm.login = login;
-  vm.isLoggedIn = isLoggedIn;
-  vm.isAdmin = isAdmin;
-  vm.setOwner = setOwner;
-  vm.logout = logout;
-  vm.getHeaders = getHeaders;
-  vm.getAllOwners = getAllOwners;
-  vm.newOwner = newOwner;
-  vm.newComment = newComment;
-  vm.getAllComments = getAllComments;
+  // vm.isLoggedIn = isLoggedIn;
+  // vm.isAdmin = isAdmin;
+  // vm.setOwner = setOwner;
+  // vm.logout = logout;
+  // vm.getHeaders = getHeaders;
+  // vm.getAllOwners = getAllOwners;
+  // vm.newOwner = newOwner;
+  // vm.newComment = newComment;
+  // vm.getAllComments = getAllComments;
 
 
-  function login (owner) {
-    return $http.post(`${SERVER}login`, owner);
+  function login (user) {
+    return $http.post(`${SERVER}login`, user);
   }
 
-  function isLoggedIn () {
-    return $cookies.get('username') ? true : false;
-  }
-
-  function isAdmin () {
-    return $cookies.get('admin') === 'true';
-  }
-
-  function logout () {
-    $cookies.remove('username');
-    $cookies.remove('access_token');
-    $cookies.remove('admin');
-  }
-
-  function setOwner (data) {
-    $cookies.put('username', data.username);
-    $cookies.put('access_token', data.access_token);
-    $cookies.put('admin', data.admin);
-  }
-
-  function getOwner (id) {
-    return $http.get(`${SERVER}/owners/${id}`)
-  }
-
-  function getAllOwners (){
-    return $http.get(`${SERVER}owners`)
-  }
-
-  function getAllComments (){
-    return $http.get(`${SERVER}comments`)
-  }
-
-  function getHeaders () {
-    let token = $cookies.get('access_token');
-    return {
-      Authorization: `Bearer ${token}`
-    };
-  }
-
-  function newOwner (info){
-    let req = {
-      url: `${SERVER}owners`,
-      data: info,
-      method: 'POST',
-      headers: vm.getHeaders()
-    };
-    return $http(req)
-  }
-
-    function newComment (info){
-      let req = {
-        url: `${SERVER}comments`,
-        data: info,
-        method: 'POST',
-        headers: vm.getHeaders()
-      };
-    return $http(req)
-  }
+  // function isLoggedIn () {
+  //   return $cookies.get('username') ? true : false;
+  // }
+  //
+  // function isAdmin () {
+  //   return $cookies.get('admin') === 'true';
+  // }
+  //
+  // function logout () {
+  //   $cookies.remove('username');
+  //   $cookies.remove('access_token');
+  //   $cookies.remove('admin');
+  // }
+  //
+  // function setOwner (data) {
+  //   $cookies.put('username', data.username);
+  //   $cookies.put('access_token', data.access_token);
+  //   $cookies.put('admin', data.admin);
+  // }
+  //
+  // function getOwner (id) {
+  //   return $http.get(`${SERVER}/owners/${id}`)
+  // }
+  //
+  // function getAllOwners (){
+  //   return $http.get(`${SERVER}owners`)
+  // }
+  //
+  // function getAllComments (){
+  //   return $http.get(`${SERVER}comments`)
+  // }
+  //
+  // function getHeaders () {
+  //   let token = $cookies.get('access_token');
+  //   return {
+  //     Authorization: `Bearer ${token}`
+  //   };
+  // }
+  //
+  // function newUser (info){
+  //   let req = {
+  //     url: `${SERVER}owners`,
+  //     data: info,
+  //     method: 'POST',
+  //     headers: vm.getHeaders()
+  //   };
+  //   return $http(req)
+  // }
+  //
+  //   function newComment (info){
+  //     let req = {
+  //       url: `${SERVER}comments`,
+  //       data: info,
+  //       method: 'POST',
+  //       headers: vm.getHeaders()
+  //     };
+  //   return $http(req)
+  // }
 
 };
 
