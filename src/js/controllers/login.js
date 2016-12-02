58a3307fe6e29f8e52842708cf1c8f6bc300f836
Jack Storrs <jackstorrs8@gmail.com>
2016-12-02 11:17:18 -0500

    Uncomment content of UsersService and start defining a login function on our LoginController.

--- src/js/controllers/login.js
+++ src/js/controllers/login.js
@@ -1,5 +1,11 @@
-function LoginController () {
+function LoginController (UsersService) {
+  let vm = this;
+  vm.login = login;
+
+  function login(username, password) {
+
+  }
 
 }
-LoginController.$inject = []
+LoginController.$inject = ['UsersService']
 export {LoginController}
