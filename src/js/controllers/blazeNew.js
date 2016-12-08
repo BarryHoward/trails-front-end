769596650848f061f4ab43bf67e9b41241a094f2
Barry Howard <BarryLeeHoward@gmail.com>
2016-12-07 23:05:38 -0500

    fixed white window bug

--- src/js/controllers/blazeNew.js
+++ src/js/controllers/blazeNew.js
@@ -3,6 +3,13 @@ function BlazeNewController (MapsService, UsersService, $scope, $state) {
   const mapId = "blazeNewMap"
   let vm =this;
 
+  //functions
+  vm.placeMarker = placeMarker;
+  vm.newTrail = newTrail;
+
+  //Maps Service
+  vm.MapsService = MapsService;
+
   //params specific to page
   MapsService.delete = false;
   MapsService.insert = "backInsert";
@@ -10,21 +17,15 @@ function BlazeNewController (MapsService, UsersService, $scope, $state) {
   MapsService.snap = false;
   MapsService.markerArray = [];
   MapsService.panel={};
-  MapsService.trailPath = [];
   MapsService.regraphElevation = true;
-  MapsService.trailInfo ={};
+  MapsService.trailPath = [];
 
-  vm.MapsService = MapsService;
-  vm.placeMarker = placeMarker;
-  vm.newTrail = newTrail;
-
+  //Constants
   const metersFeetConversion = 3.28084;
   const metersMilesConversion = 0.000621371;
   const encoding = google.maps.geometry.encoding;
   const spherical = google.maps.geometry.spherical;
   const Geocoder = new google.maps.Geocoder();
-
-
 
   init();
 
@@ -41,8 +42,8 @@ function BlazeNewController (MapsService, UsersService, $scope, $state) {
         MapsService.createTrailPoly();
         MapsService.initSearch();
         MapsService.map.setMapTypeId('terrain');
-        var infoWindow = new google.maps.InfoWindow({map: MapsService.map});
-        someFunction(infoWindow);
+        // var infoWindow = new google.maps.InfoWindow({map: MapsService.map});
+        centerByLocation();
     })
   }
 
@@ -70,40 +71,18 @@ function BlazeNewController (MapsService, UsersService, $scope, $state) {
 // Jack's weird centering shit -----------------------------------
 
 
-  function someFunction(infoWindow){
-      if (navigator.geolocation) {
+  function centerByLocation(infoWindow){
+    if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
         var pos = {
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
-        infoWindow.setPosition(pos);
         MapsService.map.setCenter(pos);
-      }, function() {
-        handleLocationError(true, infoWindow, MapsService.map.getCenter());
-      });
-    } else {
-      handleLocationError(false, infoWindow, MapsService.map.getCenter());
+      })
     }
   }
 
-  function geocodeAddress() {
-    Geocoder.geocode({'address': vm.address}, function(results, status) {
-          if (status === 'OK') {
-            MapsService.map.setCenter(results[0].geometry.location);
-            MapsService.map.setZoom(6);
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-        });
-  }
-
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-      }
 
 // -----------------------------------------
 
