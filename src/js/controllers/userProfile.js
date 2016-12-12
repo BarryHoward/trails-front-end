d3d65bc5a6d8fc8e988ae913eb20c533d11bb3ae
Jack Storrs <jackstorrs8@gmail.com>
2016-12-12 17:07:36 -0500

    Get user-profile page to start talking to back end.

--- src/js/controllers/userProfile.js
+++ src/js/controllers/userProfile.js
@@ -1,26 +1,38 @@
 function UserProfileController ($state, UsersService, MapsService) {
   let vm = this;
-  vm.changeUsername = changeUsername;
   vm.updatingUsername = false;
   vm.updatingEmail = false;
   vm.updatingAvatar = false;
+  vm.changeUsername = changeUsername;
+  vm.changeEmail = changeEmail;
+  vm.changeAvatar = changeAvatar;
+  vm.UsersService = UsersService;
+  vm.user_id = UsersService.currentUserId();
+  vm.editUser = editUser;
 
   function changeUsername () {
     vm.updatingUsername = !vm.updatingUsername;
     console.log(vm.updatingUsername);
-    return updatingUsername;
   }
 
   function changeEmail () {
     vm.updatingEmail = !vm.updatingEmail;
     console.log(vm.updatingEmail);
-    return updatingEmail;
   }
 
   function changeAvatar () {
     vm.updatingAvatar = !vm.updatingAvatar;
     console.log(vm.updatingAvatar);
-    return updatingAvatar;
+  }
+
+  function editUser(user){
+    console.log('clicked!')
+    console.log(user)
+    UsersService.editUser(user).then((resp) =>{
+      console.log(resp)
+    }, (reject) => {
+      console.log(reject)
+    })
   }
 }
 UserProfileController.$inject = ['$state', 'UsersService', 'MapsService'];
